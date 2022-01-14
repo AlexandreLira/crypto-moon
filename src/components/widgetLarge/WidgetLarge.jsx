ea867e8fe5f207155cf57a0b9b83b141e4815476
Alexandre Lira <alexandrelira112@gmail.com>
2022-01-14 13:41:40 -0300

    Widget Large

--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
 import "./widgetLarge.css";
 
-export default function WidgetLarge(){
-    return(
+export default function WidgetLarge() {
+
+    const latestTransactions = [
+        {
+            name: "Anna Keller",
+            title: "Soft Enginner",
+            image: "https://images.pexels.com/photos/10013070/pexels-photo-10013070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            date: "2 Jun 2021",
+            amount: 128.38,
+            status: "Approved"
+        },
+        {
+            name: "Diane O'Gallagher",
+            title: "Design",
+            image: "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            date: "10 Jun 2021",
+            amount: 135.82,
+            status: "Declined"
+        },
+        {
+            name: "Diane O'Gallagher",
+            title: "Design",
+            image: "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            date: "10 Jun 2021",
+            amount: 135.82,
+            status: "Pending"
+        },
+    ];
+
+    const Button = ({ type }) => {
+        return (
+            <button className={"widgetLargeButton " + type}>{type}</button>
+        )
+    }
+
+    return (
         <div className="widgetLarge">
-            large
+            <h3 className="widgetLargeTitle">Latest transactions</h3>
+            <table className="widgetLargeTable">
+                <tr className="widgetLargeTr">
+                    <th className="widgetLargeTh">Customer</th>
+                    <th className="widgetLargeTh">Date</th>
+                    <th className="widgetLargeTh">Amount</th>
+                    <th className="widgetLargeTh">Status</th>
+                </tr>
+                {latestTransactions.map((item, index) => (
+                    <tr className="widgetLargeTr" key={index}>
+                        <td className="widgetLargeUser">
+                            <img
+                                src={item.image}
+                                alt="profile"
+                                className="widgetLargeImage"
+                            />
+                            <span className="widgetLargeName">{item.name}</span>
+                        </td>
+                        <td className="widgetLargeDate">{item.date}</td>
+                        <td className="widgetLargeAmount">R${item.amount}</td>
+                        <td className="widgetLargeStatus"><Button type={item.status} /></td>
+                    </tr>
+                ))}
+            </table>
         </div>
     );
 }
--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -1,63 +1,63 @@
-import { Visibility } from '@material-ui/icons';
-import React from 'react';
+import { Visibility } from "@material-ui/icons";
+import React from "react";
 import "./widgetSmall.css";
 
-export default function WidgetSmall(){
-
+export default function WidgetSmall() {
     const newMembers = [
         {
             name: "Anna Keller",
             title: "Soft Enginner",
-            image: "https://images.pexels.com/photos/10013070/pexels-photo-10013070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/10013070/pexels-photo-10013070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
         {
             name: "Diane O'Gallagher",
             title: "Design",
-            image: "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
         {
             name: "Ailish Mercado",
             title: "Product Manager",
-            image: "https://images.pexels.com/photos/9780689/pexels-photo-9780689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/9780689/pexels-photo-9780689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
         {
             name: "Anna Keller",
             title: "Soft Enginner",
-            image: "https://images.pexels.com/photos/10013070/pexels-photo-10013070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/10013070/pexels-photo-10013070.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
         {
             name: "Diane O'Gallagher",
             title: "Design",
-            image: "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/9948310/pexels-photo-9948310.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
         {
             name: "Ailish Mercado",
             title: "Product Manager",
-            image: "https://images.pexels.com/photos/9780689/pexels-photo-9780689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            image:
+                "https://images.pexels.com/photos/9780689/pexels-photo-9780689.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         },
-        
-    ]
+    ];
 
-    return(
+    return (
         <div className="widgetSmall">
             <span className="widgetSmallTitle">New Join Members</span>
             <ul className="widgetSmallList">
                 {newMembers.map((item, index) => (
                     <li className="widgetSmallItem" key={index}>
-                    <img 
-                        src={item.image} 
-                        alt="profile" 
-                        className="widgetSmallImage"
-                    />
-                    <div className="widgetSmallUser">
-                        <span className="widgetSmallUserName">{item.name}</span>
-                        <span className="widgetSmallUserTitle">{item.title}</span>
-                    </div>
-                    <button className="widgetSmallButton">
-                        <Visibility/>
-                        Display
-                    </button>
-                </li>
+                        <img src={item.image} alt="profile" className="widgetSmallImage" />
+                        <div className="widgetSmallUser">
+                            <span className="widgetSmallUserName">{item.name}</span>
+                            <span className="widgetSmallUserTitle">{item.title}</span>
+                        </div>
+                        <button className="widgetSmallButton">
+                            <Visibility />
+                            Display
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
